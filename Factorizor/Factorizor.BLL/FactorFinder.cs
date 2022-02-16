8eba3909b271ddd378dfc398b8fbeac40ed45aee
ramyagawad <97934923+ramyagawad@users.noreply.github.com>
2022-02-16 02:33:51 -0500

    Add files via upload

---
+++ Factorizor/Factorizor.BLL/FactorFinder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorizor.BLL
+{
+    public class FactorFinder
+    {
+        public int[] FindFactors(int num, out int numfactors)
+        {
+            int[] facts = new int[80]; // size of 80 is arbitrary
+            int i, j;
+            // Find factors and put them in the facts array.
+            for (i = 2, j = 0; i < num / 2 + 1; i++)
+            {
+                if ((num % i) == 0)
+                {
+                    facts[j] = i;
+                    j++;
+                }
+            }
+            numfactors = j;
+            return facts;
+
+        }
+    }
+}
---
+++ Factorizor/Factorizor.BLL/PerfectChecker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorizor.BLL
+{
+    class PerfectChecker
+    {
+        public static Boolean perfectNumber(int number)
+        {
+            int perfectNum = 0;
+
+            for (int i = 1; i < number; i++)
+            {
+
+                if (number % i == 0)
+                    perfectNum += i;
+            }
+            if (perfectNum == number)
+            {
+                return true;
+                //Console.Write("\n{0} is a perfect number.\n", number);
+            }
+            else
+            {
+                return false;
+                //Console.Write("\n{0} is not a perfect number.\n", number);
+            }
+
+        }
+    }
+}
